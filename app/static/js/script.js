cf49ae6f42867be8085460df0466eda50ad5f834
SARA SIMON <smbsimon@gmail.com>
2016-09-07 13:39:15 -0400

    call me hemingway, i just added a novel

---
+++ app/static/js/script.js
@@ -0,0 +1,23 @@
+$(".newsheader").click(function() {
+  $('.newscontent').slideToggle().toggleClass('active');
+});
+
+$(".musicheader").click(function() {
+  $('.musiccontent').slideToggle().toggleClass('active');
+});
+
+$(".buildingheader").click(function() {
+  $('.buildingcontent').slideToggle().toggleClass('active');
+});
+
+$(".studiooneheader").click(function() {
+  $('.studioonecontent').slideToggle().toggleClass('active');
+});
+
+$(".hqheader").click(function() {
+  $('.hqcontent').slideToggle().toggleClass('active');
+});
+
+$(".sustainabilityheader").click(function() {
+  $('.sustainabilitycontent').slideToggle().toggleClass('active');
+});
